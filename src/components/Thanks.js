176dd49d501fe8c4edf8b39aec16cb2989b5f5a4
Maha-Qunoot <mahaqunoot21@gmail.com>
2024-01-13 20:43:27 +0500

    Third commit

--- src/components/Thanks.js
+++ src/components/Thanks.js
@@ -4,7 +4,7 @@ const Thanks = () => {
   return (
     <section className="thanks">
       <h3>Thank you for your time reviewing my work on the movie ticket booking website</h3>
-      <a href="#" className="next-case-study">Next Case Study</a>
+      
     </section>
   );
 };
